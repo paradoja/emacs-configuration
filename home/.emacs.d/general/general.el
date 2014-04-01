109931dd445924c7fdbd3fb2763e5d2613289549
Abby Henríquez Tejera <paradoja@gmail.com>
2014-04-01 13:38:34 +0100

    Added flx-ido mode.

--- home/.emacs.d/general/general.el
+++ home/.emacs.d/general/general.el
@@ -59,6 +59,8 @@
 (require 'ido)
 (ido-mode t)
 (ido-everywhere t)
+(flx-ido-mode t)
+(setq ido-use-faces nil)
 (setq ido-enable-flex-matching t)
 (setq ido-use-filename-at-point 'guess)
 (setq ido-auto-merge-work-directories-length -1)
