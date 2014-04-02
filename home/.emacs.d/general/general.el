8a907654d703a9876957cdef3563a675b4d594a7
Abby Henríquez Tejera <paradoja@gmail.com>
2014-04-02 14:58:43 +0100

    Removed flx-ido.

--- home/.emacs.d/general/general.el
+++ home/.emacs.d/general/general.el
@@ -59,8 +59,6 @@
 (require 'ido)
 (ido-mode t)
 (ido-everywhere t)
-(flx-ido-mode t)
-(setq ido-use-faces nil)
 (setq ido-enable-flex-matching t)
 (setq ido-use-filename-at-point 'guess)
 (setq ido-auto-merge-work-directories-length -1)
