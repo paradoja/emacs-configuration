69896b378e435a35d7b5d46dc5561234993d1a99
Abby Henríquez Tejera <paradoja@gmail.com>
2014-04-03 15:58:36 +0100

    C-, works as it should everywhere else in org-mode (magit-status).

--- home/.emacs.d/tools/org-mode.el
+++ home/.emacs.d/tools/org-mode.el
@@ -8,3 +8,4 @@
 (setq-default org-confirm-babel-evaluate nil)
 (setq-default org-ditaa-jar-path (locate-file "ditaa" exec-path '("" ".jar")))
 (setq-default org-plantuml-jar-path (locate-file "plantuml" exec-path '("" ".jar")))
+(define-key org-mode-map (kbd "C-,") 'magit-status)
