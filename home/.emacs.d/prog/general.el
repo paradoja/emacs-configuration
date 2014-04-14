920f58c739328bab0eef120cdf9c27ea71b7ffa5
Abby Henríquez Tejera <paradoja@gmail.com>
2014-04-14 13:50:58 +0100

    Configuring ediff.

--- home/.emacs.d/prog/general.el
+++ home/.emacs.d/prog/general.el
@@ -2,3 +2,8 @@
   (interactive "p")
   (newline arg)
   (indent-according-to-mode))
+
+;;; Ediff
+(setq-default ediff-window-setup-function 'ediff-setup-windows-plain)
+(setq-default ediff-split-window-function 'split-window-horizontally)
+(setq-default ediff-auto-refine 'on)
