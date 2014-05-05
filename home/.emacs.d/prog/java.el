466a8d0aa3d81cdbf4e68a9599ccf6d78378a8a7
Abby Henríquez Tejera <paradoja@gmail.com>
2014-05-05 15:30:35 +0100

    Added default workspaces for eclim.

--- home/.emacs.d/prog/java.el
+++ home/.emacs.d/prog/java.el
@@ -1,7 +1,17 @@
 (require 'eclim)
 (require 'eclimd)
 (global-eclim-mode)
+(defvar eclim-possible-workspaces '("~/curro"))
+
 (add-to-list 'eclim-eclipse-dirs "~/opt/eclipse")
+
+(require 'f)
+(require 'dash)
+(let ((valid-paths (-filter #'f-exists?
+                            eclim-possible-workspaces)))
+  (when (not (null valid-paths))
+    (setq-default eclimd-default-workspace
+                  (car valid-paths))))
 
 (require 'ac-emacs-eclim-source)
 (ac-emacs-eclim-config)
