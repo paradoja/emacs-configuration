e4535ae5ba9a2933f5b2f63e728689a19dc1ab27
Abby Henríquez Tejera <paradoja@gmail.com>
2015-03-14 23:41:13 +0000

    Added key-chord for hl-line-mode

--- home/.emacs.d/general/key-chords.el
+++ home/.emacs.d/general/key-chords.el
@@ -4,5 +4,6 @@
 (key-chord-define-global "º," 'comment-or-uncomment-region)
 (key-chord-define-global "ºv" 'evil-mode)
 (key-chord-define-global "ºh" 'toggle-hightlight-indentation-mode)
+(key-chord-define-global "ºl" 'hl-line-mode)
 (key-chord-define-global "ºj" 'ace-jump-mode)
 (key-chord-define-global "ºk" 'ace-jump-char-mode)
