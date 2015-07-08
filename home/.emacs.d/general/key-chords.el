2fdd3ae443781d9ab9aaf3ace3e396fb10c06372
Abby Henríquez Tejera <abby.henriquez@wimdu.com>
2015-07-08 11:13:01 +0200

    Set keychords for < as well as º

--- home/.emacs.d/general/key-chords.el
+++ home/.emacs.d/general/key-chords.el
@@ -1,10 +1,17 @@
-(key-chord-define-global "ºm" 'universal-argument) ; C-u... for those moments
-(key-chord-define-global "ºu" 'universal-argument) ; C-u... for those moments
-(key-chord-define-global "ºi" 'indent-region)
-(key-chord-define-global "º," 'comment-or-uncomment-region)
-(key-chord-define-global "ºv" 'evil-mode)
-(key-chord-define-global "ºh" 'toggle-hightlight-indentation-mode)
-(key-chord-define-global "ºl" 'hl-line-mode)
-(key-chord-define-global "ºj" 'ace-jump-mode)
-(key-chord-define-global "ºk" 'ace-jump-char-mode)
-(key-chord-define-global "º " 'whitespace-mode)
+(defvar key-chord-prefixes '("º" "<"))
+
+(defun set-chords-for-key (key)
+  "Sets key chords for given keys"
+
+  (key-chord-define-global (concat key "m") 'universal-argument) ; C-u... for those moments
+  (key-chord-define-global (concat key "u") 'universal-argument) ; C-u... for those moments
+  (key-chord-define-global (concat key "i") 'indent-region)
+  (key-chord-define-global (concat key ",") 'comment-or-uncomment-region)
+  (key-chord-define-global (concat key "v") 'evil-mode)
+  (key-chord-define-global (concat key "h") 'toggle-hightlight-indentation-mode)
+  (key-chord-define-global (concat key "l") 'hl-line-mode)
+  (key-chord-define-global (concat key "j") 'aace-jump-mode)
+  (key-chord-define-global (concat key "k") 'ace-jump-char-mode)
+  (key-chord-define-global (concat key " ") 'whitespace-mode))
+
+(mapc #'set-chords-for-key key-chord-prefixes)
