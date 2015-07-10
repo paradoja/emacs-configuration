8d5e24331e8ee123837818867ff1ceddd086a5ad
Abby Henríquez Tejera <abby.henriquez@wimdu.com>
2015-07-10 11:48:23 +0200

    Changed projectile's switch project action

--- home/.emacs.d/tools/projectile.el
+++ home/.emacs.d/tools/projectile.el
@@ -9,3 +9,16 @@
 ; Removing Projectile from the mode line
 (setq-default projectile-mode-line "")
 (defun projectile-update-mode-line ())
+
+; Change default switch-project to select open buffers if there are
+; any, instead of find-file
+(defun custom-projectile-open-project-buffer-or-file (&optional arg)
+  "If the project has any open buffers, it will try to open one
+  of them. Otherwise it will prompt to open a project file."
+  (interactive "P")
+  (if (eq (projectile-project-buffer-names) '())
+      (projectile-find-file arg)
+    (projectile-switch-to-buffer)))
+
+(setq projectile-switch-project-action
+      'custom-projectile-open-project-buffer-or-file)
