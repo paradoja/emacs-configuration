49284092a77be9623a80a39764cacb6b0437d4f0
Abby Henríquez Tejera <paradoja@gmail.com>
2015-07-27 00:17:44 +0200

    Updated Clojure packages

--- home/.emacs.d/prog/clojure.el
+++ home/.emacs.d/prog/clojure.el
@@ -2,29 +2,8 @@
 (add-hook 'clojure-mode-hook
            (lambda () (paredit-mode +1)))
 
-; nREPL
-(add-hook 'nrepl-interaction-mode-hook
-          'nrepl-turn-on-eldoc-mode)
-(setq nrepl-hide-special-buffers t)
-(add-hook 'nrepl-mode-hook 'paredit-mode)
-(add-hook 'nrepl-mode-hook 'subword-mode)
-(add-hook 'nrepl-mode-hook 'rainbow-delimiters-mode)
+(add-hook 'cider-mode-hook #'eldoc-mode)
 
-(defun my-nrepl-mode-setup ()
-  (require 'nrepl-ritz))
-(add-hook 'nrepl-interaction-mode-hook 'my-nrepl-mode-setup)
-
-; Compojure
-(add-hook 'clojure-mode-hook
-          (lambda () (define-clojure-indent
-                       (defroutes 'defun)
-                       (GET 2)
-                       (POST 2)
-                       (PUT 2)
-                       (DELETE 2)
-                       (HEAD 2)
-                       (ANY 2)
-                       (context 2))))
 
 ;; ;; highlight expression on eval
 ;; (require 'highlight)
