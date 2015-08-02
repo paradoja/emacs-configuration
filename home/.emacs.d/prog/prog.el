2ce010e155671dea7282656cf2f79ad6bed19ba1
Abby Henríquez Tejera <paradoja@gmail.com>
2015-08-02 17:38:18 +0200

    Added Rust packages

--- home/.emacs.d/prog/prog.el
+++ home/.emacs.d/prog/prog.el
@@ -1,41 +1,44 @@
 ;;; General
 (load-file "~/.emacs.d/prog/general.el")
 
-;;; elisp
-(load-file "~/.emacs.d/prog/elisp.el")
-
-;;; Common Lisp
-(load-file "~/.emacs.d/prog/clisp.el")
-
-;;; Scheme && Racket
-(load-file "~/.emacs.d/prog/scheme.el")
+;;; C
+(load-file "~/.emacs.d/prog/c.el")
 
 ;;; Clojure
 (load-file "~/.emacs.d/prog/clojure.el")
 
-;;; Ruby, Rails && Co.
-(load-file "~/.emacs.d/prog/ruby.el")
+;;; CoffeeScript
+(load-file "~/.emacs.d/prog/coffee.el")
+
+;;; Common Lisp
+(load-file "~/.emacs.d/prog/clisp.el")
+
+;;; elisp
+(load-file "~/.emacs.d/prog/elisp.el")
 
 ;;; Haskell
 (load-file "~/.emacs.d/prog/haskell.el")
 
-;;; C
-(load-file "~/.emacs.d/prog/c.el")
-
-;;; SCSS
-(load-file "~/.emacs.d/prog/scss.el")
+;;; Java
+(load-file "~/.emacs.d/prog/java.el")
 
 ;;; (n)ASM
 (load-file "~/.emacs.d/prog/nasm.el")
 
+;;; Ruby, Rails && Co.
+(load-file "~/.emacs.d/prog/ruby.el")
+
+;;; Rust
+(load-file "~/.emacs.d/prog/rust.el")
+
 ;;; Scala
 (load-file "~/.emacs.d/prog/scala.el")
 
+;;; SCSS
+(load-file "~/.emacs.d/prog/scss.el")
+
+;;; Scheme && Racket
+(load-file "~/.emacs.d/prog/scheme.el")
+
 ;;; Web-mode
 (load-file "~/.emacs.d/prog/web-mode.el")
-
-;;; Java
-(load-file "~/.emacs.d/prog/java.el")
-
-;;; CoffeeScript
-(load-file "~/.emacs.d/prog/coffee.el")
