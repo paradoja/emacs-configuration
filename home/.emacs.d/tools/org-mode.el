de5f9044aba256e679f43fdff4dcbbe55664b596
Abby Henríquez Tejera <abby.henriquez@wimdu.com>
2016-04-08 12:15:32 +0200

    Fixes to configuration in org-mode

--- home/.emacs.d/tools/org-mode.el
+++ home/.emacs.d/tools/org-mode.el
@@ -1,11 +1,20 @@
+(require 'org)
+
 (org-babel-do-load-languages
  'org-babel-load-languages
  '((emacs-lisp . t)
    (ruby . t)
    (ditaa . t)
-   (plantuml . t)))
+   (plantuml . t)
+   (sh . t)))
 
+; Key map changes
+(define-key org-mode-map (kbd "C-,") 'magit-status)
+
+; Source blocks
 (setq-default org-confirm-babel-evaluate nil)
+(setq org-src-fontify-natively t) ; set up syntax highlighting in source blocks
+
+; Diagrams
 (setq-default org-ditaa-jar-path (locate-file "ditaa" exec-path '("" ".jar")))
 (setq-default org-plantuml-jar-path (locate-file "plantuml" exec-path '("" ".jar")))
-(define-key org-mode-map (kbd "C-,") 'magit-status)
