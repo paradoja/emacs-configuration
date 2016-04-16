585ed3c36e06c36183a8cfaa3a2798955d2b0938
Abby Henríquez Tejera <paradoja@gmail.com>
2016-04-16 18:34:36 +0200

    Added auto-yasnippet

--- home/.emacs.d/tools/yasnippets.el
+++ home/.emacs.d/tools/yasnippets.el
@@ -1,3 +1,10 @@
 (require 'yasnippet)
 (add-to-list 'yas-snippet-dirs "~/.emacs.d/tools/yasnippets/")
 (yas-global-mode 1)
+
+(require 'use-package)
+
+(use-package auto-yasnippet
+  :bind (("<backtab> w" . aya-create)
+         ("<backtab> y" . aya-expand)
+         ("<backtab> o" . aya-open-line)))
