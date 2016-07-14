a30bdff67674bc022c1c44d5d30080fa8bf6ce40
Abby Henríquez Tejera <abby.henriquez@wimdu.com>
2016-07-14 21:02:40 +0200

    Configured notmuch

--- home/.emacs.d/tools/email.el
+++ home/.emacs.d/tools/email.el
@@ -5,6 +5,8 @@
 ;;; notmuch (also installable outside emacs) is used to index these.
 ;;; The notmuch emacs client provides a minimal emacs client.
 
+;;; https://notmuchmail.org/emacstips/
+
 ;;; M-x notmuch
 ;;; Everywhere
 ;;;  `-> q go back
@@ -19,7 +21,32 @@
 ;;;  `-> R reply
 ;;;  `-> F forward
 
-(use-package notmuch)
+;;; TODO: Signature
+
+(use-package notmuch
+  :bind (:map notmuch-hello-mode-map
+         ("R" . reload-email)))
+
+(require 's) ; TODO: use-package it
+
+; TODO : this should probably not be here
+(defmacro run-after-process (process-list func)
+  `(let ((first-process ,process-list))
+     ; TODO: add (process-status process-list) check in case the process finished
+     (set-process-sentinel first-process
+                           (lambda (_process event)
+                             (if (string= (s-trim event) "finished")
+                                 ,func)))))
+
+(defun reload-email ()
+  (interactive)
+  (run-after-process (start-process "sync email" "*Messages*" "mbsync" "gmail")
+                     (run-after-process
+                      (start-process "reindex email" "*Messages*" "notmuch" "new")
+                      (progn
+                        (notmuch-poll-and-refresh-this-buffer)
+                        (message "Email updated")))))
 
 (setq notmuch-search-oldest-first nil ; newer first
-      )
+      message-citation-line-format "On %a, %d %b %Y, %f wrote:"
+      message-citation-line-function 'message-insert-formatted-citation-line)
